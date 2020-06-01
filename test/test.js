28909fcd21d880c786cbb0f607124c9ad85f0f19
zhangrh <zhangrhweb@163.com>
2020-06-01 13:35:03 +0800

    fix bug

--- test/test.js
+++ test/test.js
@@ -3,9 +3,9 @@ const app = getApp()
 Page({
   data: {
     content: [
-      "你好你好我不号, 镰刀锤子你好东方红东方红红红",
-      "东方阿斯顿罚款三123你好123段论法大夫埃里克森调节阀, 东方阿什克隆东方骄傲三等分角你好",
-      "阿历山大减肥, 你好辣椒粉撒旦六份东方红"
+      "你好你好东方红我东方红不号, 镰刀锤子你好东方红东方红红红",
+      "东方红阿斯顿罚款三123你好123段论法大夫埃里克森调节阀, 东方阿什克隆东方骄傲三等分角你好",
+      "阿历山东方红大减肥, 辣你椒号   你好粉撒旦六份东方红"
     ],
     key: ["你好", "东方红"]
   },
@@ -17,17 +17,24 @@ Page({
     this.setData({
       finallyArray,
     })
-    console.log(finallyArray)
   },
   handleKeyLine(context, keyArray) {
     // init
     let array = []
-    let key = keyArray[0]
+
+    let key = ""
+    let keyIndex = 0
     let hasKey = false
     keyArray.forEach(item => {
-      if (context.indexOf(item) > -1 && !hasKey) {
-        key = item
-        hasKey = true
+      if (context.indexOf(item) > -1) {
+        let index = context.indexOf(item)
+        // !hasKey: 排除第一次刚进来的情况, 索引就不是0了
+        // keyIndex >= index: 排查到更加考前的地方
+        if (keyIndex >= index || !hasKey) {
+          keyIndex = index
+          key = item
+          hasKey = true
+        }
       }
     });
 
@@ -44,13 +51,22 @@ Page({
         type: "vr",
         text: context.substr(index, key.length)
       })
-
       context = context.slice(index + key.length, context.length)
+
+      
+      keyIndex = 0
       hasKey = false
+      key = ""
       keyArray.forEach(item => {
-        if (context.indexOf(item) > -1 && !hasKey) {
-          key = item
-          hasKey = true
+        if (context.indexOf(item) > -1) {
+          let index = context.indexOf(item)
+          // !hasKey: 排除第一次刚进来的情况, 索引就不是0了
+          // keyIndex >= index: 排查到更加考前的地方
+          if (keyIndex >= index || !hasKey) {
+            keyIndex = index
+            key = item
+            hasKey = true
+          }
         }
       });
     }
